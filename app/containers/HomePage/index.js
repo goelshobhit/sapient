d802c8958c78a1e9e022c5ba0304db75e28e8068
strieveParrot <shobhitgoel27@gmail.com>
2020-09-15 01:27:03 +0530

    chore: fix lint

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -61,7 +61,13 @@ export function HomePage({ OnRequestSpaceData, homePage: { data, loading } }) {
       </Helmet>
 
       <div className="container">
-        <Typography variant="h3" component="h1" gutterBottom strong="true" data-aos="fade-up">
+        <Typography
+          variant="h3"
+          component="h1"
+          gutterBottom
+          strong="true"
+          data-aos="fade-up"
+        >
           <FormattedMessage {...messages.header} />
         </Typography>
         <Grid
