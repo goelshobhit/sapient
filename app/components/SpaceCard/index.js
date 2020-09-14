88fea5771c4cf23af7629bd8fcfd78520c239b9e
strieveParrot <shobhitgoel27@gmail.com>
2020-09-15 02:08:46 +0530

    chore: fix image issue

--- app/components/SpaceCard/index.js
+++ app/components/SpaceCard/index.js
@@ -8,6 +8,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import { last, isEmpty } from 'lodash';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -43,51 +45,65 @@ const useStyles = makeStyles({
 
 function SpaceCard({ data }) {
   const classes = useStyles();
+  const {
+    links: { flickr_images, mission_patch_small, video_link },
+  } = data;
+  function renderImage() {
+    if (!isEmpty(flickr_images)) {
+      return last(flickr_images);
+    }
+    return mission_patch_small;
+  }
+
   return (
-    <Card className={classes.root}>
-      <CardMedia
-        component="img"
-        className={classes.media}
-        src={data.links.mission_patch}
-        title="Contemplative Reptile"
-      />
-      <Typography
-        gutterBottom
-        variant="h5"
-        className={classes.heading}
-        component="p"
-        data-aos="flip-right"
-      >
-        {data.mission_name}
-      </Typography>
-      <Typography
-        variant="body2"
-        gutterBottom
-        component="div"
-        data-aos="flip-right"
-      >
-        <div className={classes.title}>Launch Year</div>{' '}
-        <div className={classes.subTitle}>{data.launch_year}</div>
-      </Typography>
-      <Typography
-        variant="body2"
-        gutterBottom
-        component="div"
-        data-aos="flip-right"
-      >
-        <div className={classes.title}>SuccessFul Launch</div>{' '}
-        <div className={classes.subTitle}>{data.launch_success.toString()}</div>
-      </Typography>
-      <Typography
-        variant="body2"
-        gutterBottom
-        component="div"
-        data-aos="flip-right"
-      >
-        <div className={classes.title}>SuccessFul Landing</div>{' '}
-        <div className={classes.subTitle}>{data.launch_site.site_name}</div>
-      </Typography>
-    </Card>
+    <Link href={video_link}>
+      <Card className={classes.root}>
+        <CardMedia
+          component="img"
+          className={classes.media}
+          src={renderImage()}
+          title={data.mission_name}
+        />
+        <Typography
+          gutterBottom
+          variant="h5"
+          className={classes.heading}
+          component="p"
+          data-aos="flip-right"
+        >
+          {data.mission_name}
+        </Typography>
+        <Typography
+          variant="body2"
+          gutterBottom
+          component="div"
+          data-aos="flip-right"
+        >
+          <div className={classes.title}>Launch Year</div>{' '}
+          <div className={classes.subTitle}>{data.launch_year}</div>
+        </Typography>
+        <Typography
+          variant="body2"
+          gutterBottom
+          component="div"
+          data-aos="flip-right"
+        >
+          <div className={classes.title}>SuccessFul Launch</div>{' '}
+          <div className={classes.subTitle}>
+            {data.launch_success.toString()}
+          </div>
+        </Typography>
+        <Typography
+          variant="body2"
+          gutterBottom
+          component="div"
+          data-aos="flip-right"
+        >
+          <div className={classes.title}>SuccessFul Landing</div>{' '}
+          <div className={classes.subTitle}>{data.launch_site.site_name}</div>
+        </Typography>
+      </Card>
+    </Link>
   );
 }
 
