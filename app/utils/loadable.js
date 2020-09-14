5560cb91b7f1d2cea5d3d6764c8775fa46a03da6
strieveParrot <shobhitgoel27@gmail.com>
2020-09-14 11:49:36 +0530

    chore: fix loadable util

--- app/utils/loadable.js
+++ app/utils/loadable.js
@@ -1,7 +1,8 @@
-
+import React from 'react';
 import dynamic from 'next/dynamic';
 import Loader from 'components/Loader';
 
-const loadable = importFunc => dynamic(importFunc, loading: () => <Loader />);
+const loadable = importFunc =>
+  dynamic(importFunc, { loading: () => <Loader /> });
 
 export default loadable;
